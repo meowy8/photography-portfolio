04a8f3f1efbb5fab97b1f6d99d5f4d2ea48167bd
meowy8 <mhoey8@hotmail.com>
2024-02-25 22:03:38 +0000

    initial commit

---
+++ src/app/components/SiteHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SiteHeader = () => {
+  return (
+    <header className='text-xl flex justify-center w-full p-2'>Micheal Hoey</header>
+  )
+}
+
+export default SiteHeader
